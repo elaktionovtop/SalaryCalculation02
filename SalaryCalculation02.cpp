5ae2110f2c3e63028de9e2937f511a5e80bd1c18
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2025-10-01 09:08:47 +0300

    Базовый класс

--- SalaryCalculation02.cpp
+++ SalaryCalculation02.cpp
@@ -3,40 +3,87 @@ using namespace std;
 
 class Employee
 {
+private:
+    string firstName;
+    string lastName;
 public:
-    void display() const
+    Employee(const string& firstName, const string& lastName)
+        : firstName(firstName), lastName(lastName)
     {
-        cout << "!!!" << endl;
     }
 
+    void setFirstName(const string& firstName)
+    {
+        this->firstName = firstName;
+    }
+
+    string getFirstName() const
+    {
+        return firstName;
+    }
+
+    void setLastName(const string& lastName)
+    {
+        this->lastName = lastName;
+    }
+
+    string getLastName() const
+    {
+        return lastName;
+    }
+
+    void display() const
+    {
+        cout << getFirstName() << " " << getLastName() << endl;
+    }
 };
 
 class Worker : public Employee
 {
+public:
+    Worker(const string& firstName, const string& lastName)
+        : Employee(firstName, lastName)
+    {
+    }
 };
 
 class SalaryWorker : public Employee
 {
+public:
+    SalaryWorker(const string& firstName, const string& lastName)
+        : Employee(firstName, lastName)
+    {
+    }
 };
 
 class Salesman : public SalaryWorker
 {
+public:
+    Salesman(const string& firstName, const string& lastName)
+        : SalaryWorker(firstName, lastName)
+    {
+    }
 };
 
 class Manager : public SalaryWorker
 {
+public:
+    Manager(const string& firstName, const string& lastName)
+        : SalaryWorker(firstName, lastName)
+    {
+    }
 };
 
 int main()
 {
     Employee* employees[7];
-    employees[0] = new Worker();
-    employees[1] = new Worker();
-    employees[2] = new Salesman();
-    employees[3] = new Salesman();
-    employees[4] = new Manager();
-    employees[5] = new Manager();
-    employees[6] = new Manager();
+    employees[0] = new Worker("Kate", "Jordan");
+    employees[1] = new Worker("Jack", "Peterson");
+    employees[2] = new Salesman("Peter", "Brown");
+    employees[3] = new Salesman("Antony", "Norton");
+    employees[4] = new Manager("David", "Flynn");
+    employees[5] = new Manager("Marian", "Wood");
+    employees[6] = new Manager("Brent", "Simpson");
 
     for (auto item : employees)
     {
