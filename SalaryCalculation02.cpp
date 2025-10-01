315d4f7b1c6c39b444043644a0aeb76e20354f29
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2025-10-01 09:26:21 +0300

    Повременная оплата

--- SalaryCalculation02.cpp
+++ SalaryCalculation02.cpp
@@ -32,7 +32,9 @@ public:
         return lastName;
     }
 
-    void display() const
+    virtual double calculateSalary() const = 0;
+
+    virtual void display() const
     {
         cout << getFirstName() << " " << getLastName() << endl;
     }
@@ -40,10 +42,55 @@ public:
 
 class Worker : public Employee
 {
+private:
+    const int hoursLimit = 40;
+    int  hoursNumber;
+    double hourlyRate;
 public:
-    Worker(const string& firstName, const string& lastName)
+    Worker(const string& firstName, const string& lastName,
+        int hoursNumber, double hourlyRate)
         : Employee(firstName, lastName)
     {
+        setHoursNumber(hoursNumber);
+        setHourlyRate(hourlyRate);
+    }
+    void setHourlyRate(double hourlyRate)
+    {
+        this->hourlyRate = hourlyRate < 0.0 ? 0.0 : hourlyRate;
+    }
+
+    double getHourlyRate() const
+    {
+        return hourlyRate;
+    }
+
+    void setHoursNumber(double hoursNumber)
+    {
+        this->hoursNumber = hoursNumber < 0.0 ? 0.0 : hoursNumber;
+    }
+
+    double getHoursNumber() const
+    {
+        return hoursNumber;
+    }
+
+    double calculateSalary() const override
+    {
+        double salary = getHourlyRate() * getHoursNumber();
+        if (getHoursNumber() > hoursLimit)
+        {
+            salary += (getHoursNumber() - hoursLimit) * getHourlyRate();
+        }
+        return  salary;
+    }
+
+    void display() const override
+    {
+        Employee::display();
+        cout << 
+            "hours number: " << getHoursNumber() <<
+            ", hourly rate: " << getHourlyRate() <<
+            ", month salary: " << calculateSalary() << endl;
     }
 };
 
@@ -63,6 +110,11 @@ public:
         : SalaryWorker(firstName, lastName)
     {
     }
+
+    double calculateSalary() const override
+    {
+        return 0;
+    }
 };
 
 class Manager : public SalaryWorker
@@ -72,13 +124,18 @@ public:
         : SalaryWorker(firstName, lastName)
     {
     }
+
+    double calculateSalary() const override
+    {
+        return 0;
+    }
 };
 
 int main()
 {
     Employee* employees[7];
-    employees[0] = new Worker("Kate", "Jordan");
-    employees[1] = new Worker("Jack", "Peterson");
+    employees[0] = new Worker("Kate", "Jordan", 30, 10);
+    employees[1] = new Worker("Jack", "Peterson", 50, 10);
     employees[2] = new Salesman("Peter", "Brown");
     employees[3] = new Salesman("Antony", "Norton");
     employees[4] = new Manager("David", "Flynn");
